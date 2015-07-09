3a0429b36ed30836e7583482297a031c7743f9e3
Andy Bennett <andybennett@Andys-MacBook-Pro.local>
2015-07-09 17:24:53 +0100

    Create framework

---
+++ PiwikTrackerFramework/PiwikTrackerFramework.h
@@ -0,0 +1,19 @@
+//
+//  PiwikTrackerFramework.h
+//  PiwikTrackerFramework
+//
+//  Created by Andy Bennett on 09/07/2015.
+//  Copyright (c) 2015 Mattias Levin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PiwikTrackerFramework.
+FOUNDATION_EXPORT double PiwikTrackerFrameworkVersionNumber;
+
+//! Project version string for PiwikTrackerFramework.
+FOUNDATION_EXPORT const unsigned char PiwikTrackerFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PiwikTrackerFramework/PublicHeader.h>
+
+
